59150c739fd05476737d4f8e905f99a7dc52ecf9
kere <kere@keres-MacBook-Air.local>
2023-09-02 17:11:30 +0400

    create components folder and display questions

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,9 +1,14 @@
-function StartScreen({ numQuestions }) {
+function StartScreen({ numQuestions, dispatch }) {
   return (
     <div className="start">
       <p>Welcome to The React Quiz!</p>
       <h3>{numQuestions} questions to test your React mastery</h3>
-      <button className="btn btn-ui">Let's Start</button>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Let's Start
+      </button>
     </div>
   );
 }
