595757cd2f2f2b1a0f8557addf80787e5e3cbdf9
kere <kere@keres-MacBook-Air.local>
2023-09-03 21:11:14 +0400

    restart a quiz

--- src/components/App.js
+++ src/components/App.js
@@ -53,6 +53,12 @@ function reducer(state, action) {
         highScore:
           state.points > state.highScore ? state.points : state.highScore,
       };
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+      };
     default:
       throw new Error("Action unknown");
   }
@@ -112,6 +118,7 @@ export default function App() {
             points={points}
             maxPossiblePoints={maxPossiblePoints}
             highScore={highScore}
+            dispatch={dispatch}
           />
         )}
       </Main>
